847552b17507a43952e1aed4ea6055b57b2dd8ee
Julián Rojas <juliandra140201@gmail.com>
2023-06-22 23:04:39 -0500

    Work of Today

---
+++ src/components/Presentation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, CardBody, CardImg, CardTitle } from 'reactstrap'
+
+export default function Presentation() {
+  return (
+    <>
+        <div className='text-center'>
+            <Card>
+                {/* <CardImg top src='../img/logo.png'></CardImg> */}
+                <CardBody>
+                    <CardTitle>
+                        <h1 className='strong'>Portal Restaurante</h1>
+                        <h3 className='font-weight normal font-italic'>Nuestro lema es servir con agrado al público</h3>
+                    </CardTitle>
+                    <CardImg src='logo.png' alt='Logo' />
+                </CardBody>
+            </Card>
+        </div>
+    </>
+  )
+}
